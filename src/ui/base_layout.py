e0c28f86c3b88a9754f057c88b1339e1496725ab
Yogesh Singh Kharkwal <yogeshkharkwal1@gmail.com>
2026-07-14 15:37:17 +0530

    solve the mistakes

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -47,11 +47,11 @@ def style_base_layout():
         @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
 
                 
-        #  /* Hide Top Bar of streamlit */
+         /* Hide Top Bar of streamlit */
                 
-        #     #MainMenu, footer, header {
-        #         visibility: hidden;
-        #     }
+            #MainMenu, footer, header {
+                visibility: hidden;
+            }
                 
             .block-container {
                 padding-top:1.5rem !important;    
